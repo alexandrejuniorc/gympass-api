ab8293c6f3f2771e38c59991e3a1af3947a1f9b5
Alexandre Junior <alejunior.dev@gmail.com>
2025-04-29 12:06:02 -0300

    feat: implements profile controller and jwt middleware

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,8 +1,13 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile.use-case.factory'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  console.log(request.user)
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({ userId: request.user.sub })
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, camelcase
+  const { password_hash, ...userWithoutPassword } = user
+
+  return reply.status(200).send({ user: userWithoutPassword })
 }

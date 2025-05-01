28811f92105cd81aa0aeeff0f8bb0e6b5a9b7318
Alexandre Junior <alejunior.dev@gmail.com>
2025-05-01 16:26:42 -0300

    feat: create test e2e environment and organize npm scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,27 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
+  test: {
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'E2E',
+          environment: 'prisma',
+          include: ['src/http/controllers/**/*.{test,spec}.{js,ts}'],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'UNIT',
+          environment: 'node',
+          include: [
+            'src/**/*.{test,spec}.{js,ts}',
+            '!src/http/controllers/**/*',
+          ],
+        },
+      },
+    ],
+  },
 })
